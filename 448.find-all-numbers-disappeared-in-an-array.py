bbdf34f25033ef080437682df1aaf3616f18ac80
Bin Xia <b.xia@vbreathe.com>
2023-11-20 21:50:12 +1100

    implemented findDisappearedNumbers using a hash set

--- 448.find-all-numbers-disappeared-in-an-array.py
+++ 448.find-all-numbers-disappeared-in-an-array.py
@@ -7,6 +7,16 @@
 # @lc code=start
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        numSet = set()
+        ans = []
+        for num in nums:
+            numSet.add(num)
+        
+        for i in range(1, len(nums) + 1):
+            if i not in numSet:
+                ans.append(i)
+        
+        return ans
         
 # @lc code=end
 
